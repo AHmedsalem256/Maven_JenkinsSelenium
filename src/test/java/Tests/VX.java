87f6757cc0b00347b7f2f2c43438fa08ee3d0679
= <=>
2025-10-21 21:22:23 +0300

    Adding the Testng.xml File with adding some modifications

---
+++ src/test/java/Tests/VX.java
@@ -0,0 +1,12 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class VX {
+
+    @Test
+    public void H1(){
+        System.out.println("Hello World From Ahmed Salem Jenkins");
+
+    }
+}
